fd4ab4df5713a26e423e141d3cca4a5cbbfd9813
otabuzzman <iuergen.schuck@gmail.com>
2020-01-30 20:11:42 +0100

    Make Runner exec as daemon and tty program

--- web/WEB-INF/Runner.sh
+++ web/WEB-INF/Runner.sh
@@ -1,4 +1,4 @@
-# Runner.sh - process chart creation requests
+# Runner.sh - process chart requests. Store and exec in WEB-INF directory.
 #
 
 this=$(basename $0)
@@ -8,55 +8,87 @@ this=$(basename $0)
 ( LOGLEVEL=3 info started )
 
 # mind order
-probeJRE || { fail "Java not available. Start canceled." ; exit 1 ; }
-probeBASDIR || { fail "BASDIR '$BASDIR' invalid. Start canceled" ; exit 1 ; }
-probeOUTDIR || { fail "OUTDIR '$OUTDIR' invalid. Start canceled" ; exit 1 ; }
-probeAPPDIR || { fail "APPDIR '$APPDIR' invalid. Start canceled." ; exit 1 ; }
-probeAPPEXE || { fail "APPEXE '$APPEXE' invalid. Start canceled." ; exit 1 ; }
-probeGS || { fail "Ghostscript not available. Start canceled." ; exit 1 ; }
+checkNPrc || { fail "Maximum of $NPRC instances already running." ; exit 1 ; }
 
-trap signalhandler 1 2 3 6 15
+probeJRE || { fail "Java not found." ; exit 1 ; }
+probeBASDIR || { fail "BASDIR '$BASDIR' invalid." ; exit 1 ; }
+probeDB || { fail "database '$DBURL' not available." ; exit 1 ; }
+probeOUTDIR || { fail "OUTDIR '$OUTDIR' invalid." ; exit 1 ; }
 
-while true ; do
-	probeDB || { fail "database '$DBURL' not available. Will try again..." ; sleep ${INTERVAL:-1} ; continue ; }
-	# lookup oldest chart creation request in 'accepted' state
-	# `stat` column is workaround for CRLF output of H2 Shell Tool
+probeAPPDIR || { fail "APPDIR '$APPDIR' invalid." ; exit 1 ; }
+probeAPPEXE || { fail "APPEXE '$APPEXE' invalid." ; exit 1 ; }
+probeGS || { fail "Ghostscript not found." ; exit 1 ; }
+
+trap termrnnr 1 2 3 6 15
+
+doit() {
+	# lookup chart requests in 'accepted' state sorted by oldest first
 	creq=$(java -cp $CLASSPATH -Dh2.baseDir=$BASDIR org.h2.tools.Shell \
 	-url $DBURL -user $DBUSER -password $DBPASS \
-	-sql "SELECT TOP 1 id, name, stat FROM charts WHERE stat = 'accepted' ORDER BY created ASC" |\
-	gawk '$1~/[0-9A-Za-z]{8}/ {print $0}')
+	-sql "SELECT id, name, stat FROM charts WHERE stat = 'accepted' ORDER BY created ASC" |\
+	gawk '$1~/[0-9A-Za-z]{8}/ {print $1 " " $3}')
 
 	# check and log
-	[ -n "$creq" ] || { info "nothing to do" ; sleep ${INTERVAL:-1} ; continue ; }
+	[ -n "$creq" ] \
+	&& { set $creq ; info "$(( $# / 2 )) record(s) found" ; } \
+	|| { info "nothing to do" ; return 0 ; }
 
-	set $creq ; id=$1 name=$3
-	bas=$OUTDIR/$id/$name
-	xml=$bas.xml
+	while test $# -gt 0 ; do
+		id=$1 name=$2 ; shift 2
+		bas=$OUTDIR/$id/$name
+		xml=$bas.xml
 
-	# check and log
-	[ -s $xml ] && info "found chart definition file $xml" \
-	|| { warn "chart definition file $xml missing" ; updateDB $id failed || fail "database problem occurred with $id. Cleanup manually." ; continue ; }
+		# check and log
+		[ -s $xml ] && info "found chart definition file $xml" \
+		|| { warn "chart definition file $xml missing" ; updateDB $id failed || fail "database problem occurred with $id." ; continue ; }
 
-	pdf=$bas.pdf
-	log=$bas.log
-	err=$bas.err
+		pdf=$bas.pdf
+		log=$bas.log
+		err=$bas.err
 
-	# set 'started' state
-	info "set state 'started' for $id"
-	updateDB $id started
+		# set 'started' state
+		info "set state 'started' for $id"
+		updateDB $id started || fail "database problem occurred with $id."
 
-	info "about to exec '$APPEXE $xml' ..."
-	# run Charta Caeli app
-	( cd $APPDIR ; ./$APPEXE $xml 2>$log |\
-	$GS -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=$pdf -_ >$err 2>&1 ; (( ! (${PIPESTATUS[0]}>0 || ${PIPESTATUS[1]}>0) )) ; exit $? ) \
-	&& ( info "...success" ; updateDB $id finished || fail "database problem occurred with $id. Cleanup manually." ) \
-	|| ( warn "...failure" ; updateDB $id failed || fail "database problem occurred with $id. Cleanup manually." )
+		info "running '$APPEXE $xml' on request $id ..."
+		# run Charta Caeli app
+		( cd $APPDIR ; ./$APPEXE $xml 2>$log |\
+		$GS -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=$pdf -_ >$err 2>&1 ; (( ! (${PIPESTATUS[0]}>0 || ${PIPESTATUS[1]}>0) )) ; exit $? ) \
+		&& ( info "request $id successfully processed." ; updateDB $id finished || fail "database problem occurred with $id." ) \
+		|| ( warn "failed to process request $id." ; updateDB $id failed || fail "database problem occurred with $id." )
 
-	# remove empty log files
-	[ -s $log ] || rm -f $log
-	[ -s $err ] || rm -f $err
+		# remove empty log files
+		[ -s $log ] || rm -f $log
+		[ -s $err ] || rm -f $err
 
-	unset id name
-	unset xml pdf
-	unset log err
+		unset id name
+		unset xml pdf
+		unset log err
+	done
+
+	return 0
+}
+
+loop=1
+snzz=0
+
+while getopts "i:" opt ; do
+	case $opt in
+		i)
+		test $OPTARG -eq $OPTARG 2>/dev/null \
+		&& { loop=0 ; snzz=$OPTARG ; }
+		;;
+		*)
+		exit 1
+		;;
+	esac
 done
+shift $((OPTIND-1))
+
+if test $loop -eq 0 ; then
+	while sleep $snzz ; do doit ; done
+else
+	doit
+fi
+
+exit $?
